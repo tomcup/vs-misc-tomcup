13c6e80f393424ff3c216ef358dac2b537db7871
tomcup <jiligulu7755@outlook.com>
2023-07-22 17:11:44 +0800

    finish

---
+++ MiscTomcup.cpp
@@ -0,0 +1,82 @@
+﻿#include "VapourSynth4.h"
+#include <memory>
+#include <set>
+#include <string>
+#include <filesystem>
+#include "csv.hpp"
+
+struct SCDetectData {
+private:
+	const VSAPI* vsapi;
+public:
+	VSNode* node{ nullptr };
+	std::set<int> scdata;
+
+	explicit SCDetectData(const VSAPI* vsapi) noexcept : vsapi(vsapi) {
+
+	}
+
+	~SCDetectData() {
+		vsapi->freeNode(node);
+	}
+};
+
+
+static const VSFrame* VS_CC filterGetFrame(int n, int activationReason, void* instanceData, void** frameData, VSFrameContext* frameCtx, VSCore* core, const VSAPI* vsapi) {
+	SCDetectData* d = static_cast<SCDetectData*>(instanceData);
+
+	if (activationReason == arInitial) {
+		vsapi->requestFrameFilter(n, d->node, frameCtx);
+	}
+	else if (activationReason == arAllFramesReady) {
+		const VSFrame* frame = vsapi->getFrameFilter(n, d->node, frameCtx);
+
+		VSFrame* dst = vsapi->copyFrame(frame, core);
+		VSMap* rwprops = vsapi->getFramePropertiesRW(dst);
+		vsapi->mapSetInt(rwprops, "_SceneChangeNext", d->scdata.find(n) != d->scdata.end(), maReplace);
+
+		vsapi->freeFrame(frame);
+
+		return dst;
+	}
+
+	return nullptr;
+}
+
+static void VS_CC filterFree(void* instanceData, VSCore* core, const VSAPI* vsapi) {
+	delete reinterpret_cast<SCDetectData*>(instanceData);
+}
+
+static void VS_CC filterCreate(const VSMap* in, VSMap* out, void* userData, VSCore* core, const VSAPI* vsapi) {
+	auto data{ std::make_unique<SCDetectData>(vsapi) };
+
+	data->node = vsapi->mapGetNode(in, "clip", 0, nullptr);
+
+	const std::string_view scfile{vsapi->mapGetData(in, "scfile", 0, nullptr)};
+	std::filesystem::path scfilepath{scfile};
+	if (scfilepath.is_relative())
+		scfilepath = std::filesystem::path(vsapi->getPluginPath(vsapi->getPluginByID("com.tomcup.SCDetect", core))).remove_filename().append(scfile);
+	const std::filesystem::directory_entry scfileEntry{scfilepath};
+
+	const VSVideoInfo* vi = vsapi->getVideoInfo(data->node);
+	try {
+		if (!scfileEntry.exists())
+			throw std::runtime_error(std::string("target scfile not exists: ") + scfilepath.generic_string());
+		csv::CSVReader reader(scfilepath.generic_string());
+		for (csv::CSVRow& row : reader) {
+			data->scdata.insert(row["End Frame"].get<int>() - 1);
+		}
+	}
+	catch (const std::runtime_error& e) {
+		vsapi->mapSetError(out, (std::string("SCDetect: ") + e.what()).c_str());
+		return;
+	}
+
+	VSFilterDependency deps[] = { {data->node, rpGeneral} };
+	vsapi->createVideoFilter(out, "SCDetect", vi, filterGetFrame, filterFree, fmParallel, deps, 1, data.release(), core);
+}
+
+VS_EXTERNAL_API(void) VapourSynthPluginInit2(VSPlugin* plugin, const VSPLUGINAPI* vspapi) {
+	vspapi->configPlugin("com.tomcup.SCDetect", "tomcup", "Mark the clip with PySceneDetect's output", VS_MAKE_VERSION(1, 0), VAPOURSYNTH_API_VERSION, 0, plugin);
+	vspapi->registerFunction("SCDetect", "clip:vnode;scfile:data", "clip:vnode;", filterCreate, 0, plugin);
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # vs-misc-tomcup
+`PySceneDetect` v0.6.1
+
+`VapourSynthx64` R63
+
+# com.tomcup.SCDetect
+项目受到 [vs-miscfilters-obsolete](https://github.com/vapoursynth/vs-miscfilters-obsolete) 启发
+
+专门对 [VapourSynth-RIFE-ncnn-Vulkan](https://github.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan) 中转场功能的改进，所以仅仅实现了  [vs-miscfilters-obsolete](https://github.com/vapoursynth/vs-miscfilters-obsolete)  中 `SCDetect` 函数的半边（`_SceneChangeNext`）
+
+由于 [vs-miscfilters-obsolete](https://github.com/vapoursynth/vs-miscfilters-obsolete) 中的 `SCDetect` 方法被 [VapourSynth-RIFE-ncnn-Vulkan](https://github.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan) 所使用，但其转场识别效果不理想。故使用 `PySceneDetect` 进行转场识别，根据其输出模仿 [vs-miscfilters-obsolete](https://github.com/vapoursynth/vs-miscfilters-obsolete) 中的 `SCDetect` 方法对视频转场进行标识。
+
+## 代码解释
+1. 在该项目实现的过程中，由于 `PySceneDetect` 尚未发布正式版（v1），故该项目可能会在未来（您的现在）无法正常读取其输出的 `csv` 文件。
+2. 根据实验，`VapourSynthx64` R63 以第 0 帧作为起始帧，`PySceneDetect` v0.6.1 以第 1 帧作为起始帧，故在代码实现中要对 `PySceneDetect` 的输出进行减 1 操作。 
+
+# 感谢以下项目
+[csv-parser](https://github.com/vincentlaucsb/csv-parser)
+
+# 备注
+开发者不积极维护此项目，如果发现问题，很可能无法及时回复。
+
+如需联系，请使用邮箱。开发者多半会在节假日进行回复。（原因略）
